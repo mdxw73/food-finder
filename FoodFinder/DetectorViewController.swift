75276ee1ccbbbd87050c891770adeaadbf077501
Zack Obied <zackobied@gmail.com>
2023-08-27 10:25:58 +0100

    fixed bounding boxes

--- FoodFinder/DetectorViewController.swift
+++ FoodFinder/DetectorViewController.swift
@@ -273,19 +273,26 @@ extension UIImageView {
     var contentClippingRect: CGRect {
         guard let image = image else { return bounds }
         guard contentMode == .scaleAspectFit else { return bounds }
-        guard image.size.width > 0 && image.size.height > 0 else { return bounds }
-        
-        let scale: CGFloat
-        if image.size.width > image.size.height {
-            scale = bounds.width / image.size.width
+
+        let imageSize = image.size
+        let imageAspectRatio = imageSize.width / imageSize.height
+        let viewAspectRatio = bounds.width / bounds.height
+
+        var scaledSize: CGSize = .zero
+        var origin: CGPoint = .zero
+
+        if imageAspectRatio > viewAspectRatio {
+            // Fit horizontally
+            scaledSize.width = bounds.width
+            scaledSize.height = bounds.width / imageAspectRatio
+            origin.y = (bounds.height - scaledSize.height) / 2.0
         } else {
-            scale = bounds.height / image.size.height
+            // Fit vertically or perfectly
+            scaledSize.height = bounds.height
+            scaledSize.width = bounds.height * imageAspectRatio
+            origin.x = (bounds.width - scaledSize.width) / 2.0
         }
-        
-        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
-        let x = (bounds.width - size.width) / 2.0
-        let y = (bounds.height - size.height) / 2.0
-        
-        return CGRect(x: x, y: y, width: size.width, height: size.height)
+
+        return CGRect(origin: origin, size: scaledSize)
     }
 }
